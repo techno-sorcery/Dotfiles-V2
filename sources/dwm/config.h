1cd8c6cacf9a8e79a71a4870ca08ab10653a83a0
techno-sorcery <inquisitivecalculator@gmail.com>
2023-11-10 13:52:15 -0800

    added

--- sources/dwm/config.h
+++ sources/dwm/config.h
@@ -94,7 +94,7 @@ static const Layout layouts[] = {
 
 /* commands */
 /* static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL }; */
-static const char *dmenucmd[] = { "rofi", "-show", "drun", "-theme", "dmenu", "-font", dmenufont, NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", "-theme", "dmenu", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
